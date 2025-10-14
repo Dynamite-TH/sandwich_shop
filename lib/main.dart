a8b56d669251f2d56619481e8b1631e2b0eb5fbd
Dynamite-TH <tedhayward@icloud.com>
2025-10-14 13:24:49 +0100

    added column usage

--- lib/main.dart
+++ lib/main.dart
@@ -13,28 +13,68 @@ class App extends StatelessWidget {
       title: 'My Sandwich Shop',
       home: Scaffold(
         appBar: AppBar(title: const Text('Sandwich Counter')),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(8.0),
-              color: Colors.blueAccent,
-              child: OrderItemDisplay(5, 'Club'),
+        body: Center(
+          // child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  // alignment: Alignment.center,
+                  color: Colors.blue,
+                  child: OrderItemDisplay(5, 'Club'),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  color: Colors.orange,
+                  child: OrderItemDisplay(3, 'BLT'),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  color: Colors.green,
+                  child: OrderItemDisplay(2, 'Veggie'),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  color: Colors.red,
+                  child: OrderItemDisplay(6, 'Ham'),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  color: Colors.yellow,
+                  child: OrderItemDisplay(9, 'Turkey'),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  color: Colors.purple,
+                  child: OrderItemDisplay(1, 'Chicken'),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  color: Colors.pink,
+                  child: OrderItemDisplay(5, 'Jam'),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  color: Colors.red,
+                  child: OrderItemDisplay(6, 'Ham'),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  color: Colors.red,
+                  child: OrderItemDisplay(6, 'Ham'),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  color: Colors.red,
+                  child: OrderItemDisplay(6, 'Ham'),
+                ),
+              ],
             ),
-            Container(
-              padding: const EdgeInsets.all(8.0),
-              color: Colors.orange,
-              child: OrderItemDisplay(3, 'BLT'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8.0),
-              color: Colors.green,
-              child: OrderItemDisplay(2, 'Veggie'),
-            ),
-          ],
+          ),
         ),
-      ),
-    );
+      );
   }
 }
 
@@ -52,5 +92,4 @@ class OrderItemDisplay extends StatelessWidget {
     );
   }
 }
-
 
