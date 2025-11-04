37ba2cfb20a608fbb6f23253d86ecf946cf42f40
Dynamite-TH <tedhayward@icloud.com>
2025-11-04 17:31:14 +0000

    added price repository to order_repository

--- lib/repositories/order_repository.dart
+++ lib/repositories/order_repository.dart
@@ -21,3 +21,18 @@ class OrderRepository {
     }
   }
 }
+
+class PricingRepository {
+  final double sixInchPrice;
+  final double footlongPrice;
+
+  PricingRepository({this.sixInchPrice = 7.0, this.footlongPrice = 11});
+
+  double calculatePrice(int quantity, bool isFootlong) {
+    double basePrice = sixInchPrice;
+    if (isFootlong) {
+      basePrice = footlongPrice;
+    }
+    return basePrice * quantity;
+  }
+}
