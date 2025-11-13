bd63ceb8035f6723796362c758fa3e0e68c649dc
Dynamite-TH <tedhayward@icloud.com>
2025-11-13 17:36:28 +0000

    Change image fit to 'contain' for better display in OrderScreen

--- lib/main.dart
+++ lib/main.dart
@@ -188,7 +188,7 @@ class _OrderScreenState extends State<OrderScreen> {
                 height: 300,
                 child: Image.asset(
                   _getCurrentImagePath(),
-                  fit: BoxFit.cover,
+                  fit: BoxFit.contain,
                   errorBuilder: (context, error, stackTrace) {
                     return const Center(
                       child: Text('Image not found', style: normalText),
