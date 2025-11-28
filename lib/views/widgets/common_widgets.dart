28dde42ab20753fec340d5b8a493f4f254cdb7b7
Dynamite-TH <tedhayward@icloud.com>
2025-11-28 10:08:04 +0000

    add AppBarWidget for consistent app header with cart item count

---
+++ lib/views/widgets/common_widgets.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sandwich_shop/models/cart.dart';
+import 'package:sandwich_shop/views/app_styles.dart';
+
+class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const AppBarWidget(this.title, {super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  @override
+  Widget build(BuildContext context) => AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            height: 100,
+            child: Image.asset('assets/images/logo.png'),
+          ),
+        ),
+        title: Text(title, style: AppStyles.heading1),
+        actions: [
+          Consumer<Cart>(
+            builder: (context, cart, child) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Icon(Icons.shopping_cart),
+                    const SizedBox(width: 4),
+                    Text('${cart.countOfItems}'),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
+      );
+}
